fba25aa93930b30408e81a17e73484d89b429f4f
VickyPrado <vicky.prado05@gmail.com>
2025-03-28 18:21:55 -0300

    aumentando-fonte

--- ConsultaCNPJ/obj/Debug/net9.0/ConsultaCNPJ.AssemblyInfo.cs
+++ ConsultaCNPJ/obj/Debug/net9.0/ConsultaCNPJ.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsultaCNPJ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2b00fc1c4ab2e493d1850bf2b93250438a4a5a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33c1c031856b51b8e3dd6b00f4a89f76c72518ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsultaCNPJ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsultaCNPJ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
